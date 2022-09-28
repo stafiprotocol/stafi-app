80990224550a2e4fdf8b3c823bf6cba196fa9729
joe <joe@stafi.io>
2022-09-28 16:30:03 +0800

    add redirects

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/rtoken",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
