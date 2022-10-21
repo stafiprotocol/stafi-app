4d1d696dab717dfbba67a24e99fb8e19b6e0fc8b
joe <joe@stafi.io>
2022-10-21 10:48:18 +0800

    fix wording

--- components/reth/TokenStakeTabs.tsx
+++ components/reth/TokenStakeTabs.tsx
@@ -18,7 +18,7 @@ export const TokenStakeTabs = (props: TokenStakeTabsProps) => {
         )}
         onClick={() => onChange("unmatched")}
       >
-        Unmatched
+        Unstaked
       </div>
 
       <div
