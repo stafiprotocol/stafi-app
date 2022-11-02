c2445ad8c5742780e0ec72969ce87a0dddb0ce0b
joe <joewu590@gmail.com>
2022-11-02 14:52:25 +0800

    temp commit

--- components/layout/layout_validator.tsx
+++ components/layout/layout_validator.tsx
@@ -1,17 +1,20 @@
 import { getMetamaskValidatorChainId } from "config/metaMask";
 import { hooks } from "connectors/metaMask";
+import { WalletType } from "interfaces/common";
 import { useContext, useEffect } from "react";
 import { MyLayoutContext } from "./layout";
 
 type ValidatorLayoutProps = React.PropsWithChildren<{}>;
 
 export const ValidatorLayout = (props: ValidatorLayoutProps) => {
-  const { setTargetMetaMaskChainId } = useContext(MyLayoutContext);
+  const { setTargetMetaMaskChainId, setWalletType } =
+    useContext(MyLayoutContext);
   const { useChainId, useAccount } = hooks;
 
   useEffect(() => {
     setTargetMetaMaskChainId(getMetamaskValidatorChainId());
-  }, [setTargetMetaMaskChainId]);
+    setWalletType(WalletType.MetaMask);
+  }, [setTargetMetaMaskChainId, setWalletType]);
 
   return <div>{props.children}</div>;
 };
--- utils/common.ts
+++ utils/common.ts
@@ -1,4 +1,3 @@
-import { keccakAsHex } from "@polkadot/util-crypto";
 import { checkAddressChecksum } from "web3-utils";
 
 export function openLink(url: string | undefined | null) {
@@ -17,32 +16,14 @@ export function isServer() {
 }
 
 export function checkMetaMaskAddress(address: string) {
-  // check if it has the basic requirements of an address
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
     return false;
-    // If it's ALL lowercase or ALL upppercase
   } else if (
     /^(0x|0X)?[0-9a-f]{40}$/.test(address) ||
     /^(0x|0X)?[0-9A-F]{40}$/.test(address)
   ) {
     return true;
-    // Otherwise check each case
   } else {
     return checkAddressChecksum(address);
-    // address = address.replace(/^0x/i, "");
-    // var addressHash = keccakAsHex(address.toLowerCase()).substr(2);
-
-    // for (var i = 0; i < 40; i++) {
-    //   // the nth letter should be uppercase if the nth digit of casemap is 1
-    //   if (
-    //     (parseInt(addressHash[i], 16) > 7 &&
-    //       address[i].toUpperCase() !== address[i]) ||
-    //     (parseInt(addressHash[i], 16) <= 7 &&
-    //       address[i].toLowerCase() !== address[i])
-    //   ) {
-    //     return false;
-    //   }
-    // }
-    // return true;
   }
 }
