d9acba82594cc753963946b02c28265d6ceb3107
jeff2857 <jeffliu2857@gmail.com>
2022-11-22 11:11:58 +0800

    fix trade card clicking

--- components/rtoken/TradeDexCardItem.tsx
+++ components/rtoken/TradeDexCardItem.tsx
@@ -21,44 +21,47 @@ export const TradeDexCardItem = (props: TradeDexCardItemProps) => {
     <Card
       background="rgba(26, 40, 53, 0.2)"
       borderColor="#1A2835"
-      className="w-[3.35rem] relative flex flex-col items-center"
+      className="w-[3.35rem] relative flex flex-col items-center cursor-pointer"
     >
-      {props.type === "rDEX" && (
-        <div className="w-[1.59rem] h-[.91rem] absolute right-[-1.5px] top-[-1px]">
-          <Image src={recommendImage} layout="fill" alt="permissionless" />
-        </div>
-      )}
-
-      <div className="bg-[#171717] border-[1px] border-solid border-border1 w-[.76rem] h-[.76rem] rounded-full mt-[.24rem] p-[.15rem]">
-        <div className="w-full h-full relative">
-          <Image alt="icon" layout="fill" src={getDexIcon(type)} />
-        </div>
-      </div>
-
-      <GradientText size=".4rem" className="mt-[.24rem]">
-        {props.type}
-      </GradientText>
-
-      <div className="mx-[.24rem] mt-[.4rem] self-stretch flex items-center justify-between">
-        <MyTooltip
-          text="Token Standard"
-          title="The native token standard for DEX platform"
-          className="text-[.16rem] text-text2"
-        />
-
-        <div className="text-[.16rem] text-text1">{props.tokenStandard}</div>
-      </div>
-
-      <Button
-        secondary={props.type !== "rDEX"}
-        mt=".47rem"
-        height=".65rem"
-        fontSize=".24rem"
-        className="mx-[.24rem] mt-[.47rem] mb-[.32rem] self-stretch"
+      <div
         onClick={() => openLink(props.url)}
       >
-        Trade
-      </Button>
+        {props.type === "rDEX" && (
+          <div className="w-[1.59rem] h-[.91rem] absolute right-[-1.5px] top-[-1px]">
+            <Image src={recommendImage} layout="fill" alt="permissionless" />
+          </div>
+        )}
+
+        <div className="bg-[#171717] border-[1px] border-solid border-border1 w-[.76rem] h-[.76rem] rounded-full mt-[.24rem] p-[.15rem]">
+          <div className="w-full h-full relative">
+            <Image alt="icon" layout="fill" src={getDexIcon(type)} />
+          </div>
+        </div>
+
+        <GradientText size=".4rem" className="mt-[.24rem]">
+          {props.type}
+        </GradientText>
+
+        <div className="mx-[.24rem] mt-[.4rem] self-stretch flex items-center justify-between">
+          <MyTooltip
+            text="Token Standard"
+            title="The native token standard for DEX platform"
+            className="text-[.16rem] text-text2"
+          />
+
+          <div className="text-[.16rem] text-text1">{props.tokenStandard}</div>
+        </div>
+
+        <Button
+          secondary={props.type !== "rDEX"}
+          mt=".47rem"
+          height=".65rem"
+          fontSize=".24rem"
+          className="mx-[.24rem] mt-[.47rem] mb-[.32rem] self-stretch"
+        >
+          Trade
+        </Button>
+      </div>
     </Card>
   );
 };
