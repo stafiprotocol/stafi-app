954ed02c4505001c0f3314461eb70302f05273ca
joe <joe@stafi.io>
2022-12-09 10:49:24 +0800

    hide ksm&dot wallet

--- components/NavbarWallet.tsx
+++ components/NavbarWallet.tsx
@@ -397,7 +397,7 @@ export const NavbarWallet = () => {
             onClickConnect={() => clickConnectWallet(WalletType.MetaMask)}
           />
 
-          <WalletAccountItem
+          {/* <WalletAccountItem
             name="Kusama"
             walletType={WalletType.Polkadot_KSM}
             connected={ksmConnected}
@@ -415,7 +415,7 @@ export const NavbarWallet = () => {
             balance={dotBalance}
             tokenName={"DOT"}
             onClickConnect={() => clickConnectWallet(WalletType.Polkadot_DOT)}
-          />
+          /> */}
         </div>
       </Popover>
     </div>
